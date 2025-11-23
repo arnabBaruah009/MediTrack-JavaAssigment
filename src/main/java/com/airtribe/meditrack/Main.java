2a83a60dea674a2e6097e9a793cb7912fce118d2
arnabBaruah009 <baruaharnab769@gmail.com>
2025-11-24 03:48:18 +0530

    feat:searchable interface implementation

--- src/main/java/com/airtribe/meditrack/Main.java
+++ src/main/java/com/airtribe/meditrack/Main.java
@@ -5,6 +5,7 @@ import com.airtribe.meditrack.constants.Specialization;
 import com.airtribe.meditrack.entity.Doctor;
 import com.airtribe.meditrack.entity.Person;
 import com.airtribe.meditrack.service.DoctorService;
+import com.airtribe.meditrack.service.DoctorServiceWithSearchable;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -15,15 +16,14 @@ public class Main {
         Doctor doctor1 = new Doctor("Dr. John Doe", "jd@x.com", 1234567890L, "123 Main St", LocalDate.of(1980, 5, 15), Gender.MALE, "DOC-001", Specialization.CARDIOLOGIST);
         Doctor doctor2 = new Doctor("Dr. Jane Smith", "js@x.com", 9876543210L, "456 Elm St", LocalDate.of(1975, 8, 22), Gender.MALE, "DOC-002", Specialization.ENT_SPECIALIST);
 
-        DoctorService doctorService = new DoctorService();
+        DoctorServiceWithSearchable doctorService = new DoctorServiceWithSearchable();
         doctorService.addDoctor(doctor1);
         doctorService.addDoctor(doctor2);
 
         System.out.println("All Doctors:");
-        for(Doctor doc : doctorService.getAllDoctors()){
-            System.out.println("Name: " + doc.getName() + ", Specialization: " + doc.getSpecialization());
+        for(Doctor doc : doctorService.getAllDoctors()) {
+            System.out.println(doc.getName());
         }
-        Optional<Doctor> doctor = doctorService.getDoctorById("DOC-001");
-        doctor.ifPresent(doc -> System.out.println("Doctor found with ID: " + doc.getName()));
+        doctorService.searchByKeyword("j").forEach(d -> System.out.println("Found Doctor: " + d.getName()));
     }
 }
