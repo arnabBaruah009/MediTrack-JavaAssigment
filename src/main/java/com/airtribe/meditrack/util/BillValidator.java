87b735f5771a269b90db91506526e31e69ec0d7e
arnabBaruah009 <baruaharnab769@gmail.com>
2025-11-26 23:28:38 +0530

    feat: invalidbillingexception

--- src/main/java/com/airtribe/meditrack/util/BillValidator.java
+++ src/main/java/com/airtribe/meditrack/util/BillValidator.java
@@ -2,6 +2,7 @@ package com.airtribe.meditrack.util;
 
 import com.airtribe.meditrack.constants.PaymentStatus;
 import com.airtribe.meditrack.entity.Bill;
+import com.airtribe.meditrack.exceptions.InvalidBillingException;
 
 import java.time.LocalDateTime;
 
@@ -9,31 +10,31 @@ public class BillValidator {
     public static void validate(Bill bill) {
 
         if (bill == null) {
-            throw new IllegalArgumentException("Bill object cannot be null.");
+            throw new InvalidBillingException("Bill object cannot be null.");
         }
 
         if (bill.getBillId() == null || bill.getBillId() <= 0) {
-            throw new IllegalArgumentException("Bill ID must be a positive number.");
+            throw new InvalidBillingException("Bill ID must be a positive number.");
         }
 
         if (bill.getPatientId() == null || bill.getPatientId() <= 0) {
-            throw new IllegalArgumentException("Patient ID must be a positive number.");
+            throw new InvalidBillingException("Patient ID must be a positive number.");
         }
 
         if (bill.getAmount() == null || bill.getAmount() <= 0) {
-            throw new IllegalArgumentException("Amount must be greater than 0.");
+            throw new InvalidBillingException("Amount must be greater than 0.");
         }
 
         if (bill.getBillingDate() == null) {
-            throw new IllegalArgumentException("Billing date cannot be null.");
+            throw new InvalidBillingException("Billing date cannot be null.");
         }
 
         if (bill.getBillingDate().isAfter(LocalDateTime.now())) {
-            throw new IllegalArgumentException("Billing date cannot be in the future.");
+            throw new InvalidBillingException("Billing date cannot be in the future.");
         }
 
         if (bill.getPaymentStatus() == null) {
-            throw new IllegalArgumentException("Payment status cannot be null.");
+            throw new InvalidBillingException("Payment status cannot be null.");
         }
 
         validatePaymentStatus(bill.getPaymentStatus());
@@ -43,7 +44,7 @@ public class BillValidator {
         if (paymentStatus != PaymentStatus.PAID &&
                 paymentStatus != PaymentStatus.PENDING &&
                 paymentStatus != PaymentStatus.UNPAID) {
-            throw new IllegalArgumentException("Invalid payment status: " + paymentStatus);
+            throw new InvalidBillingException("Invalid payment status: " + paymentStatus);
         }
     }
 }
